9bf2b9670e35aec21393b5288f81342f8b7694df
BhoomikaShashidhar <bhoomi.dt87@gmail.com>
2016-07-14 10:33:15 +0530

    SEssion1Assignment1
    Sum

---
+++ Session1Assignment1/src/Sum.java
@@ -0,0 +1,10 @@
+
+	public class Sum {
+		public static void main (String args[])
+		{
+			int i = 10,k;
+			float j = 3.5f;
+			k =(int)(i+j);
+			System.out.println("sum of the numbers="+k);
+		}
+		}
